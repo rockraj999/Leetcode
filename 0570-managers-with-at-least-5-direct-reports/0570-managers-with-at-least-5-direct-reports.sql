584619519447efcb44ffab1f1cb7a97535440dcc
rockraj999 <121096737+rockraj999@users.noreply.github.com>
2023-04-08 17:27:48 +0530

    Time: 515 ms (61.66%), Space: 0B (100.00%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+
+
+with cte_tab as (select e2.id,count(0) as freq from Employee e1 join Employee e2 on e1.managerId=e2.id
+group by e2.id)
+select name from Employee where id in (select (case when freq>=5 then id end) as id from cte_tab)
+;
