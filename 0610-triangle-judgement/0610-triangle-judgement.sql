a3b3c9e7486b98ac6e11feb7e60b801a7886e664
rockraj999 <121096737+rockraj999@users.noreply.github.com>
2023-04-09 22:00:08 +0530

    Time: 661 ms (9.98%), Space: 0B (100.00%) - LeetHub

---
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+
+
+select *,case when abs(x)+abs(y)>abs(z) and abs(y)+abs(z)>abs(x) and abs(z)+abs(x)>abs(y) then 'Yes' else 'No' end as triangle from Triangle 
