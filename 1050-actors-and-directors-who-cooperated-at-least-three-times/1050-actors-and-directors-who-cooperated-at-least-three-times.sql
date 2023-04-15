13173c57f20f8cbf8ada5eab312f6302ff080690
rockraj999 <121096737+rockraj999@users.noreply.github.com>
2023-04-15 17:51:19 +0530

    Time: 633 ms (79.69%), Space: 0B (100.00%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,3 +1,6 @@
 # Write your MySQL query statement below
 
-select actor_id,director_id from (select actor_id,director_id,count(1) as freq from ActorDirector group by actor_id,director_id) t where freq>=3
+#select actor_id,director_id from (select actor_id,director_id,count(1) as freq from #ActorDirector group by actor_id,director_id) t where freq>=3
+
+
+select actor_id,director_id from ActorDirector group by actor_id,director_id having count(*)>=3;
