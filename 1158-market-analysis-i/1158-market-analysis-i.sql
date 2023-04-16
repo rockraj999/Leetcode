9896ef599a29e38b4aeb1893d3f721c353c0bf9e
rockraj999 <121096737+rockraj999@users.noreply.github.com>
2023-04-16 12:19:39 +0530

    Time: 2179 ms (80.70%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -5,6 +5,11 @@
 
 
 
-with cte_tab as (select buyer_id,count(buyer_id) as orders_in_2019 from Orders where order_date like '%2019%'
-group by buyer_id)
-select user_id as buyer_id,join_date,ifnull(orders_in_2019,0) as orders_in_2019 from Users left join cte_tab on Users.user_id=cte_tab.buyer_id;
+#with cte_tab as (select buyer_id,count(buyer_id) as orders_in_2019 from Orders where order_date like '%2019%'
+#group by buyer_id)
+#select user_id as buyer_id,join_date,ifnull(orders_in_2019,0) as orders_in_2019 from Users left join cte_tab on Users.user_id=cte_tab.buyer_id;
+
+
+select user_id as buyer_id, join_date, COUNT(order_id) as orders_in_2019 from Users LEFT JOIN Orders on user_id = buyer_id 
+and order_date >= '2019-01-01' and order_date < '2020-01-01' 
+group by user_id;;
