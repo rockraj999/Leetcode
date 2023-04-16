c0d2ac6d2757cdcf3530712fc274aacc50c06a47
rockraj999 <121096737+rockraj999@users.noreply.github.com>
2023-04-16 12:16:42 +0530

    Time: 2668 ms (23.29%), Space: 0B (100.00%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+
+#select user_id,join_date,count(buyer_id) as orders_in_2019 from Users left join Orders on  Orders.buyer_id=Users.user_id  where order_date like '%2019%' group by buyer_id
+
+
+
+with cte_tab as (select buyer_id,count(buyer_id) as orders_in_2019 from Orders where order_date like '%2019%'
+group by buyer_id)
+select user_id as buyer_id,join_date,ifnull(orders_in_2019,0) as orders_in_2019 from Users left join cte_tab on Users.user_id=cte_tab.buyer_id;
